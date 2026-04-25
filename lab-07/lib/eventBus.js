cf826e3dd2a0bdd5acf69ae1d36bba91cfcc0a46
Rostislav77-hub <rostikmatv123@gmail.com>
2026-04-25 03:14:48 +0300

    EventBus custom reactive core implemented

---
+++ lab-07/lib/eventBus.js
@@ -0,0 +1,86 @@
+'use strict';
+
+class EventBus {
+  constructor() {
+    this._events = {};
+    this._subscriptions = new Map();
+    this._nextId = 1;
+  }
+
+  subscribe(event, listener) {
+    if (!this._events[event]) {
+      this._events[event] = [];
+    }
+    this._events[event].push(listener);
+    const id = this._nextId++;
+    this._subscriptions.set(id, { event, listener });
+    return id;
+  }
+
+  subscribeOnce(event, listener) {
+    const id = this._nextId++;
+
+    const wrapper = (payload) => {
+      listener(payload);
+      this.unsubscribe(id); 
+    };
+
+    if (!this._events[event]) {
+      this._events[event] = [];
+    }
+    this._events[event].push(wrapper);
+    this._subscriptions.set(id, { event, listener: wrapper });
+
+    return id;
+  }
+
+  unsubscribe(subscriptionId) {
+    const sub = this._subscriptions.get(subscriptionId);
+    if (!sub) return false;
+
+    const { event, listener } = sub;
+    
+    if (this._events[event]) {
+      this._events[event] = this._events[event].filter(l => l !== listener);
+      
+      if (this._events[event].length === 0) {
+        delete this._events[event];
+      }
+    }
+    
+    this._subscriptions.delete(subscriptionId);
+    return true;
+  }
+
+  publish(event, payload) {
+    if (!this._events[event]) return;
+
+    const listeners = [...this._events[event]];
+    
+    listeners.forEach(listener => {
+      try {
+        listener(payload);
+      } catch (error) {
+        console.error(`Ошибка в обработчике события '${event}':`, error);
+      }
+    });
+  }
+
+  listenerCount(event) {
+    return this._events[event] ? this._events[event].length : 0;
+  }
+
+  clear(event) {
+    if (event) {
+      delete this._events[event];
+      for (const [id, sub] of this._subscriptions) {
+        if (sub.event === event) this._subscriptions.delete(id);
+      }
+    } else {
+      this._events = {};
+      this._subscriptions.clear();
+    }
+  }
+}
+
+module.exports = EventBus;
